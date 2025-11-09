1783862a24f084824c74adb7d08301e8351cc5f7
Dum4ick <duduk.maksim@gmail.com>
2025-11-10 02:02:28 +0300

    First commit

---
+++ LogisticCenter/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace LogisticCenter
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ LogisticCenter/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace LogisticCenter
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
