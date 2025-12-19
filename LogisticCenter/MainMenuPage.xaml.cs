b7c19272b81df28a6276fcdb337b789fbe819e15
Dum4ick <duduk.maksim@gmail.com>
2025-12-19 04:23:18 +0300

    Визуал для главного меню у администратора

--- LogisticCenter/MainMenuPage.xaml.cs
+++ LogisticCenter/MainMenuPage.xaml.cs
@@ -1,15 +1,13 @@
-﻿namespace LogisticCenter;
+﻿using LogisticCenter.ViewModels;
+
+namespace LogisticCenter;
 
 public partial class MainMenuPage : ContentPage
 {
     public MainMenuPage()
     {
         InitializeComponent();
-    }
-
-    private async void Logout_Clicked(object sender, EventArgs e)
-    {
-        await Shell.Current.GoToAsync("//login");
+        BindingContext = new MainMenuViewModel();
     }
 
 }
